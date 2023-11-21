acff69c49768d1e22665b1ecb3d4d30e85ee2423
Nina <138123480+ninaCapgemini@users.noreply.github.com>
2023-11-21 09:10:19 +0000

    Codepilot3rnd commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+// create web server
+var express = require('express');
+var app = express();
+
+// create server
+var server = require('http').createServer(app);
+
+// create socket.io server
+var io = require('socket.io').listen(server);
+
+// create array for users
+var users = [];
+
+// create array for connections
+var connections = [];
+
+// listen for requests
+server.listen(process.env.PORT || 3000);
+console.log('Server running...');
+
+// set up static files
+app.use(express.static('public'));
+
+// set up socket.
